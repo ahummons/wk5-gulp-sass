e0efe9f73f200258d6bc44408cb373e878a4b879
April Hummons <ahummons@gmail.com>
2018-02-10 17:06:08 -0600

    april hummons week5

--- gulpfile.js
+++ gulpfile.js
@@ -6,14 +6,18 @@ var sass = require('gulp-sass');
 
 //define a task to read your sass files and make css
 gulp.task('sass', function () {
-  return gulp.src('./src/scss/**/*.scss')
+  return gulp.src('*./scss/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./dist/css'));
 });
-
+gulp.task('sass', function() {
+  return gulp.src('./scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
+    .pipe(sass())
+    .pipe(gulp.dest('./dist/css'))
+})
 //watch task is a convience, it will rebuild your css anytime you update a sass file
 gulp.task('watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+  gulp.watch('./**/*.scss', ['sass']);
 });
 
 //default task will run when you type `gulp` at the command line
